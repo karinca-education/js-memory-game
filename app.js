7fa17114e931488de344941d655d421ce419d2c0
gulalicelik <gulalicelikmi@gmail.com>
2022-03-11 22:59:38 +0300

    beta memory game

---
+++ app.js
@@ -0,0 +1,315 @@
+const game = document.querySelector(".game");
+
+const gameCard = document.querySelectorAll(".game-card");
+
+const animals = [
+  "Aardvark",
+  "Albatross",
+  "Alligator",
+  "Alpaca",
+  "Ant",
+  "Anteater",
+  "Antelope",
+  "Ape",
+  "Armadillo",
+  "Donkey",
+  "Baboon",
+  "Badger",
+  "Barracuda",
+  "Bat",
+  "Bear",
+  "Beaver",
+  "Bee",
+  "Bison",
+  "Boar",
+  "Buffalo",
+  "Butterfly",
+  "Camel",
+  "Capybara",
+  "Caribou",
+  "Cassowary",
+  "Cat",
+  "Caterpillar",
+  "Cattle",
+  "Chamois",
+  "Cheetah",
+  "Chicken",
+  "Chimpanzee",
+  "Chinchilla",
+  "Chough",
+  "Clam",
+  "Cobra",
+  "Cockroach",
+  "Cod",
+  "Cormorant",
+  "Coyote",
+  "Crab",
+  "Crane",
+  "Crocodile",
+  "Crow",
+  "Curlew",
+  "Deer",
+  "Dinosaur",
+  "Dog",
+  "Dogfish",
+  "Dolphin",
+  "Dotterel",
+  "Dove",
+  "Dragonfly",
+  "Duck",
+  "Dugong",
+  "Dunlin",
+  "Eagle",
+  "Echidna",
+  "Eel",
+  "Eland",
+  "Elephant",
+  "Elk",
+  "Emu",
+  "Falcon",
+  "Ferret",
+  "Finch",
+  "Fish",
+  "Flamingo",
+  "Fly",
+  "Fox",
+  "Frog",
+  "Gaur",
+  "Gazelle",
+  "Gerbil",
+  "Giraffe",
+  "Gnat",
+  "Gnu",
+  "Goat",
+  "Goldfinch",
+  "Goldfish",
+  "Goose",
+  "Gorilla",
+  "Goshawk",
+  "Grasshopper",
+  "Grouse",
+  "Guanaco",
+  "Gull",
+  "Hamster",
+  "Hare",
+  "Hawk",
+  "Hedgehog",
+  "Heron",
+  "Herring",
+  "Hippopotamus",
+  "Hornet",
+  "Horse",
+  "Human",
+  "Hummingbird",
+  "Hyena",
+  "Ibex",
+  "Ibis",
+  "Jackal",
+  "Jaguar",
+  "Jay",
+  "Jellyfish",
+  "Kangaroo",
+  "Kingfisher",
+  "Koala",
+  "Kookabura",
+  "Kouprey",
+  "Kudu",
+  "Lapwing",
+  "Lark",
+  "Lemur",
+  "Leopard",
+  "Lion",
+  "Llama",
+  "Lobster",
+  "Locust",
+  "Loris",
+  "Louse",
+  "Lyrebird",
+  "Magpie",
+  "Mallard",
+  "Manatee",
+  "Mandrill",
+  "Mantis",
+  "Marten",
+  "Meerkat",
+  "Mink",
+  "Mole",
+  "Mongoose",
+  "Monkey",
+  "Moose",
+  "Mosquito",
+  "Mouse",
+  "Mule",
+  "Narwhal",
+  "Newt",
+  "Nightingale",
+  "Octopus",
+  "Okapi",
+  "Opossum",
+  "Oryx",
+  "Ostrich",
+  "Otter",
+  "Owl",
+  "Oyster",
+  "Panther",
+  "Parrot",
+  "Partridge",
+  "Peafowl",
+  "Pelican",
+  "Penguin",
+  "Pheasant",
+  "Pig",
+  "Pigeon",
+  "Pony",
+  "Porcupine",
+  "Porpoise",
+  "Quail",
+  "Quelea",
+  "Quetzal",
+  "Rabbit",
+  "Raccoon",
+  "Rail",
+  "Ram",
+  "Rat",
+  "Raven",
+  "Red deer",
+  "Red panda",
+  "Reindeer",
+  "Rhinoceros",
+  "Rook",
+  "Salamander",
+  "Salmon",
+  "Sand Dollar",
+  "Sandpiper",
+  "Sardine",
+  "Scorpion",
+  "Seahorse",
+  "Seal",
+  "Shark",
+  "Sheep",
+  "Shrew",
+  "Skunk",
+  "Snail",
+  "Snake",
+  "Sparrow",
+  "Spider",
+  "Spoonbill",
+  "Squid",
+  "Squirrel",
+  "Starling",
+  "Stingray",
+  "Stinkbug",
+  "Stork",
+  "Swallow",
+  "Swan",
+  "Tapir",
+  "Tarsier",
+  "Termite",
+  "Tiger",
+  "Toad",
+  "Trout",
+  "Turkey",
+  "Turtle",
+  "Viper",
+  "Vulture",
+  "Wallaby",
+  "Walrus",
+  "Wasp",
+  "Weasel",
+  "Whale",
+  "Wildcat",
+  "Wolf",
+  "Wolverine",
+  "Wombat",
+  "Woodcock",
+  "Woodpecker",
+  "Worm",
+  "Wren",
+  "Yak",
+  "Zebra",
+];
+const level = 2;
+const cardCounter = level * 2;
+
+var cards = [];
+
+var gameCartHTML = ``;
+var clickCounter = 0;
+var clickArray = [];
+
+const getSessionAnimals = (quantity) => {
+  const sessionAnimals = [];
+  for (let i = 0; i < quantity; i++) {
+    let currentAnimal = animals[parseInt(Math.random() * 223)];
+
+    if (!sessionAnimals.includes(currentAnimal)) {
+      sessionAnimals.push(currentAnimal);
+      sessionAnimals.push(currentAnimal);
+    } else {
+      i--;
+    }
+  }
+  return sessionAnimals;
+};
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+var gameAnimals = getSessionAnimals(level);
+gameAnimals = shuffle(gameAnimals);
+
+const generateGameCard = (cardQuantity) => {
+  for (let i = 0; i < cardQuantity; i++) {
+    var value = gameAnimals[i];
+    game.innerHTML += `
+    <div class="card-game-inner">
+    <button class="game-card ${value}  "  onclick="clickDetect('${value}',' ${i}')"><span class="card-content">${value}</span></button>
+    <button class="game-card ${value} ${i} cover "  onclick="clickDetect('${value}', '${i}')"><span class="card-content"></span></button>
+    </div>
+    `;
+  }
+};
+
+generateGameCard(cardCounter);
+
+const clickDetect = (content, index) => {
+  const cover = document.getElementsByClassName(index)[0];
+  cover.style.display = "none";
+  clickCounter++;
+  clickArray.push(content);
+
+  if (clickCounter == 2) {
+    if (clickArray[0] == clickArray[1]) {
+      var buttons = document.getElementsByClassName(content);
+
+      for (let item of buttons) {
+        item.disabled = true;
+      }
+    } else {
+      cover.style.display = "block";
+    }
+    clickCounter = 0;
+    clickArray = [];
+  } else {
+    console.log(clickCounter);
+  }
+};
+
+// BUG --> aynı karta iki kez tıklanamamalı
